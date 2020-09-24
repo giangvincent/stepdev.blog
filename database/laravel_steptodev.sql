2d8ee096877e4793f84b21f2786dcaf27bce9044
mfvodka107 <giang.dlinh92@gmail.com>
2020-09-24 16:41:43 +0700

    fix password reset too long

--- database/laravel_steptodev.sql
+++ database/laravel_steptodev.sql
@@ -609,7 +609,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 --
 
 CREATE TABLE `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
